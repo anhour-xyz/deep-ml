447dd96b92f75526ed7807be05caa8f2c35b922e
anhour-xyz <293997524+anhour-xyz@users.noreply.github.com>
2026-09-17 08:02:09 +0000

    Update: Calculate Root Mean Square Error (RMSE)

--- problems/0071-calculate-root-mean-square-error-rmse/solution.py
+++ problems/0071-calculate-root-mean-square-error-rmse/solution.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def rmse(y_true, y_pred):
 	# Write your code here
-	rmse_res = np.sqrt(np.mean((y_true - y_pred) ** 2))
+	rmse_res = np.sqrt(np.mean((y_true-y_pred) ** 2))
 	return round(rmse_res,3)
